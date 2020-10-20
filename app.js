bd689c03d4efa9375def0fa1b1c057670bd4ac94
Jeffrey Onochie <jeffreyonochie@gmail.com>
2020-10-20 08:49:07 +0100

    setup mongoose connection

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/errors');
 
-//get a DB connection - mongoConnection is d function in database.js that connects to our DB
-const mongoConnection = require('./utils/database').mongoConnection;
+const mongoose = require('mongoose');
 
 const User = require('./models/user');
 
@@ -22,14 +21,14 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // We user a middleware here to add a user to our request
-app.use((req, res, next) => {
-	User.findById('5f805f828a672e40d8ef72e0')
-	.then(user => {
-		//here we add new keys to the user data and store in our request
-		req.user = new User(user.name, user.email, user.cart, user._id);
-		next();
-	}).catch(err => console.log(err));
-})
+// app.use((req, res, next) => {
+// 	User.findById('5f805f828a672e40d8ef72e0')
+// 	.then(user => {
+// 		//here we add new keys to the user data and store in our request
+// 		req.user = new User(user.name, user.email, user.cart, user._id);
+// 		next();
+// 	}).catch(err => console.log(err));
+// })
 
 app.use(adminRoutes);
 app.use(shopRoutes);
@@ -37,7 +36,8 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 
-//REM mongoConnection() in database.js takes a callback
-mongoConnection( () => {
+mongoose.connect('mongodb+srv://jeffonochie:Audr3y321@cluster0.x6ez3.mongodb.net/localshopv2?retryWrites=true&w=majority', 
+	{useNewUrlParser: true, useUnifiedTopology: true})
+.then( () => {
 	app.listen(3000);
 })
--- routes/shop.js
+++ routes/shop.js
@@ -9,16 +9,16 @@ router.get('/', shopController.index);
 
 // router.get('/products', shopController.allProducts);
 
-router.get('/products/:productId', shopController.showProduct);
+// router.get('/products/:productId', shopController.showProduct);
 
-router.get('/cart', shopController.getCart);
+// router.get('/cart', shopController.getCart);
 
-router.post('/cart', shopController.postCart);
+// router.post('/cart', shopController.postCart);
 
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+// router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-router.get('/orders', shopController.getOrders);
-router.post('/create-order', shopController.postOrder);
+// router.get('/orders', shopController.getOrders);
+// router.post('/create-order', shopController.postOrder);
 
 // router.get('/checkout', shopController.getCheckout);
 
